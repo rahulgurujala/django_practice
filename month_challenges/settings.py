901b78b2db93848ab91e421108e221ed7aea3fcf
rahulgurujala <isaacnewtonrahul@gmail.com>
2021-06-20 20:15:10 +0530

    First working state

--- month_challenges/settings.py
+++ month_challenges/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'challenges.apps.ChallengesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
